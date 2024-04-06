9cc86b92ecfe584f6abde0a4dd4e6e8f27a1145b
Kisturyo <96877880+Kisturyo@users.noreply.github.com>
2024-04-06 21:11:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,3 @@
 
 # Add a feed source
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
