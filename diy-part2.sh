ffaa5932d6b8744c1a7615c46f65e8dd8b6a9913
Kisturyo <96877880+kisturyo@users.noreply.github.com>
2024-06-17 14:07:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # Modify hostname
-sed -i 's/OpenWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
