95f8971daf6b09e2f09dca44af0e1d50fa990579
kisturyo <honkiliang@gmail.com>
2024-12-01 10:33:00 +0800

    Update to OpenWrt 24.10

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,11 @@
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# Remove kernel version's MD5
+sed -i 's/~$(LINUX_VERMAGIC)-/-/g' include/kernel.mk
+sed -i 's/~$(LINUX_VERMAGIC)-/-/g' package/kernel/linux/Makefile
+
+# Add compilation date in OpenWrt firmware version
+date=`date +%Y.%m.%d`
+sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,16 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
 # Modify hostname
 # sed -i 's/OpenWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i "s/^.*vermagic$/\techo '1' > \$(LINUX_DIR)\/.vermagic/" include/kernel-defaults.mk
+sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci-nginx/Makefile
+
+# Nginx use http in lan
+sed -i 's/443 ssl default_server/80/g' feeds/packages/net/nginx-util/files/nginx.config
+sed -i '/uci_manage_ssl/d' feeds/packages/net/nginx-util/files/nginx.config
+sed -i '/ssl_certificate/d' feeds/packages/net/nginx-util/files/nginx.config
+sed -i '/ssl_session/d' feeds/packages/net/nginx-util/files/nginx.config
